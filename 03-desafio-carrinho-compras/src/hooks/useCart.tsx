cb150b0fbf4374903f19a791fa1fb46025d929b8
Thiago Pereira <pereira.tp.thiago@gmail.com>
2024-05-14 16:20:26 -0300

    desafio 3 - removeProduct e updateProductAmount

--- 03-desafio-carrinho-compras/src/hooks/useCart.tsx
+++ 03-desafio-carrinho-compras/src/hooks/useCart.tsx
@@ -69,9 +69,19 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
 
   const removeProduct = (productId: number) => {
     try {
-      // TODO
+      const updatedCart = [...cart]
+      const productIndex = updatedCart.findIndex(product => product.id === productId)
+      
+      if (productIndex < 0) {
+        throw Error()
+      }
+      
+      updatedCart.splice(productIndex, 1);
+      setCart(updatedCart)
+      localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart))
+
     } catch {
-      // TODO
+      toast.error('Erro na remoção do produto')
     }
   };
 
@@ -80,9 +90,30 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
     amount,
   }: UpdateProductAmount) => {
     try {
-      // TODO
+      if (amount <= 0) {
+        return
+      }
+
+      const stock = await api.get(`/stock/${productId}`)
+      const stockAmount = stock.data.amount
+
+      if (amount > stockAmount) {
+        toast.error('Quantidade solicitada fora de estoque')
+        return
+      }
+
+      const updatedCart = [...cart]
+      const productExists = updatedCart.find(product => product.id === productId)
+
+      if (!productExists) {
+        throw Error()
+      }
+
+      productExists.amount = amount
+      setCart(updatedCart)
+      localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart))
     } catch {
-      // TODO
+      toast.error('Erro na alteração de quantidade do produto')
     }
   };
 
